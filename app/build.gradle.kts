d05503219efc0c987032dda3c6e7b178549c4d22
Kaylee Hirsch <emperorshishire@gmail.com>
2026-04-06 16:42:26 -0700

    Release v0.1.3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -26,7 +26,7 @@ android {
         minSdk = 28
         targetSdk = 36
         versionCode = (System.getenv("GITHUB_RUN_NUMBER") ?: "1").toInt()
-        versionName = "0.1.2"
+        versionName = "0.1.3"
 
         buildConfigField(
             "String",
